501d6ea0cdbdfe1092fc0a42b57582092d96e7cb
manideep <saimanideep369@gmail.com>
2025-05-10 12:15:54 +0530

    changing the import path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -9,8 +8,7 @@ export default defineConfig({
     react({
       jsxRuntime: 'automatic',
       jsxImportSource: 'react'
-    }),
-    tailwindcss()
+    })
   ],
   resolve: {
     alias: {
